89d1e4597ec820d297e70fa62825c903697fd6ce
mwe3 <mwe3@njit.edu>
2018-03-30 17:40:53 -0400

    Menu creation and attempt at keypress

--- js/gallery.js
+++ js/gallery.js
@@ -160,7 +160,7 @@ $(document).ready( function() {
 	
 	// This initially hides the photos' metadata information
 	$('.details').eq(0).hide();
-	
+
 	$('.moreIndicator').click(function() {
 		console.log(".moreIndicator");
 		
@@ -189,6 +189,15 @@ $(document).ready( function() {
 	$('#prevPhoto').click(function(){
 		console.log("PREV PHOTO");
 		returnSwap();
+	});
+
+
+	$('.thumbnail').keydown(function () {
+  		if (event.keyCode == 37) {
+    		$('#prevPhoto').click(); //on left arrow, click next (since your next is on the left)
+  		} else if (event.keyCode == 39) {
+    		$('#nextPhoto').click(); //on right arrow, click prev
+  		}
 	})
 
 	
@@ -205,6 +214,27 @@ function GalleryImage(imgLocation, description, date, imgPath){
 	this.description = description;
 	this.date = date;
 	this.imgPath = imgPath;
+};
+
+	/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
 
 
